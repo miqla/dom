0aee49859d39d23c0c6133ae2137b61aa029f752
miqla <miqdad.shaldin@gmail.com>
2023-10-21 15:10:03 +0700

    menambahkan h1 untuk hitung mundur pada time.html. mengubah time.js dengan menambahkan funtion hitung mundur

--- time.js
+++ time.js
@@ -1,12 +1,60 @@
 // setTimeout(function/panggil function, waktu in milisecond)
-// setInterval()
+// menjalankan perintah setelah menunggu sesuai waktu yg telah ditentukan
 
 const timeout = setTimeout(function () {
   console.log("udah 5 detik");
 }, 5000);
 
-const tombol = document.getElementById("tombol");
-tombol.addEventListener("click", function () {
+const tombolA = document.getElementById("tombolA");
+tombolA.addEventListener("click", function () {
   clearTimeout(timeout);
   console.log("ok");
 });
+
+// setInterval(function/panggil function, waktu in milisecond)
+// menjalankan perintah setelah menunggu sesuai waktu yg telah ditentukan, tapi berulang-ulang
+
+const interval = setInterval(function () {
+  console.log("2 detik");
+}, 2000);
+
+const tombolB = document.getElementById("tombolB");
+tombolB.addEventListener("click", function () {
+  clearInterval(interval);
+  console.log("interval done");
+});
+
+// memberhentikan interval
+const berhenti = setTimeout(function () {
+  clearInterval(interval);
+  console.log("udah 10 detik");
+}, 10000);
+
+// Hitung Mundur, getTime biar bentuknya angka
+const hitungMundur = setInterval(function () {
+  const sekarang = new Date().getTime();
+  const waktuTujuan = new Date("Oct 30, 2023 14:59:59").getTime();
+  const selisih = waktuTujuan - sekarang;
+
+  const hari = Math.floor(selisih / (1000 * 60 * 60 * 24)); // satuannya milisecond
+  const jam = Math.floor((selisih % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); // 1 detik * 60 detik * 60 menit
+  const menit = Math.floor((selisih % (1000 * 60 * 60)) / (1000 * 60));
+  const detik = Math.floor((selisih % (1000 * 60)) / 1000);
+
+  const text = document.getElementById("teks");
+  text.innerHTML =
+    "Sisa waktu anda adalah: " +
+    hari +
+    " hari " +
+    jam +
+    " jam " +
+    menit +
+    " menit " +
+    detik +
+    " detik lagi";
+
+  if (selisih < 0) {
+    clearInterval(hitungMundur);
+    text.innerHTML = "Waktu Anda habis";
+  }
+}, 1000);
