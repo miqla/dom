ff6c6ccf8d1ee319653dc240d79a3a31677c99a9
miqla <miqdad.shaldin@gmail.com>
2023-10-22 12:44:23 +0700

    menambahkan cara memanfaatkan event bubble dengan memberikan event pada parentnya

--- traversal/traversal.js
+++ traversal/traversal.js
@@ -7,7 +7,7 @@
 // });
 
 // DOM Traversal (penelusuran), ex:parentElement
-const close = document.querySelectorAll(".close");
+// const close = document.querySelectorAll(".close");
 // for (let i = 0; i < close.length; i++) {
 //   close[i].addEventListener("click", function (e) {
 //     // e berisi semua informasi dari event yg sedang terjadi, coba consolelog(e)
@@ -17,14 +17,24 @@ const close = document.querySelectorAll(".close");
 // }
 
 // cara lain, el=close[i]
-close.forEach(function (el) {
-  el.addEventListener("click", function (e) {
-    e.target.parentElement.style.display = "none";
-    // e.preventDefault(); untuk mencegah aksi default dari sebuah element, misal tag a, kalo href nya kosong, defaultnya dy akan kembali ke halaman yg sama/refresh, biar dy ga refresh maka dikasih preventDefault
-    // e.stopPropagation(); untuk menghentikan event bubble dari parent nya, biar event parentnya ga berjalan di dy
-  });
-});
+// close.forEach(function (el) {
+//   el.addEventListener("click", function (e) {
+//     e.target.parentElement.style.display = "none";
+// e.preventDefault(); untuk mencegah aksi default dari sebuah element, misal tag a, kalo href nya kosong, defaultnya dy akan kembali ke halaman yg sama/refresh, biar dy ga refresh maka dikasih preventDefault
+// e.stopPropagation(); untuk menghentikan event bubble dari parent nya, biar event parentnya ga berjalan di dy
+//   });
+// });
 
 // DOM Traversal Method: parentNode -> Node, parentElement -> element, nextSibling -> node, nextElementSibling->element, previousSibling -> node, previousElementSibling->element
 // kalau mengembalikan node, bearti enter dan spasi juga dianggap
 // kalau mengembalikan element, spasi tidak dianggap, langsung fokus ke element selanjutnya
+
+// kasih event di parentnya, biar children nya ikut semua, termasuk children yg ditambahkan secara realtime akan mendapatkan efek bubble
+const container = document.querySelector(".container");
+container.addEventListener("click", function (e) {
+  // console.log(e.target);
+  if (e.target.className == "close") {
+    // jika element container yg di klik memiliki nama class close, maka jalankan perintah dibawah
+    e.target.parentElement.style.display = "none";
+  }
+});
